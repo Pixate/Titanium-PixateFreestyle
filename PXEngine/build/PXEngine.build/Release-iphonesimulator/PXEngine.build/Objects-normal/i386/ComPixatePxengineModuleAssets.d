00c1823ed0cb00e6e2fc7ed5ded03d243a7b8077
Paul Colton <paul@colton.net>
2013-01-28 13:22:11 -0800

    remove build files

--- PXEngine/build/PXEngine.build/Release-iphonesimulator/PXEngine.build/Objects-normal/i386/ComPixatePxengineModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/pcolton/Documents/Development/Pixate/ios/Integrations/Titanium/PXEngine/Classes/ComPixatePxengineModuleAssets.m \
-  /Users/pcolton/Documents/Development/Pixate/ios/Integrations/Titanium/PXEngine/Classes/ComPixatePxengineModuleAssets.h
